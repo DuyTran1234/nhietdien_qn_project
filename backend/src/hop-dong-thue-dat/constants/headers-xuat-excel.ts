b7e8c9c66819fe2bbd8c6682f6b956d670aa3013
Duy Tran <0985919093dd@gmail.com>
2026-02-03 16:50:28 +0700

    .

---
+++ backend/src/hop-dong-thue-dat/constants/headers-xuat-excel.ts
@@ -0,0 +1,14 @@
+export const columnsBangTheoDoi = [
+    { header: 'Stt', width: 5 },
+    { header: 'Số hợp đồng/ngày tháng năm', width: 25 },
+    { header: 'Quyết định cho thuê đất', width: 20 },
+    { header: 'Đơn vị tính', width: 10 },
+    { header: 'Diện tích', width: 15 },
+    { header: 'Thời gian thuê', width: 20 },
+    { header: 'Mục đích thuê', width: 40 },
+    { header: 'Khu vực thuê', width: 15 },
+    { header: 'Đơn giá thuê đất', width: 12 },
+    { header: 'Quyết định đơn giá thuê đất/ngày tháng năm', width: 15 },
+    { header: 'Thời gian ổn định đơn giá', width: 25 },
+    { header: 'Ghi chú', width: 15 },
+];
--- backend/src/hop-dong-thue-dat/service/xuat-bieu-mau.service.ts
+++ backend/src/hop-dong-thue-dat/service/xuat-bieu-mau.service.ts
@@ -1,22 +1,35 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import * as ExcelJS from 'exceljs';
-import * as path from 'path';
 import * as fs from 'fs';
-import { InjectRepository } from "@nestjs/typeorm";
-import { HopDongThueDatEntity } from "../entity/hop-dong-thue-dat.entity";
+import * as path from 'path';
 
 @Injectable()
 export class XuatBieuMauHopDong {
     constructor(
-        
+
     ) { }
     async xuatBangTheoDoi() {
         const workbook = new ExcelJS.Workbook();
         const worksheet = workbook.addWorksheet('Bảng theo dõi');
-        worksheet.mergeCells('A1:C1');
 
+        // tiêu đề công ty
+        worksheet.mergeCells('A1:C1');
+        const companyCell = worksheet.getCell('A1');
+        companyCell.value = 'Công ty cổ phần nhiệt điện Quảng Ninh';
+        companyCell.font = { bold: true, size: 14 };
+
+        // tiêu đề bảng
+        worksheet.mergeCells('A3:L3');
+        const titleCell = worksheet.getCell('A3');
+        titleCell.value = 'Bảng theo dõi các hợp đồng thuê đất';
+        titleCell.font = { bold: true, size: 14 };
+        titleCell.alignment = { vertical: 'middle', horizontal: 'center' };
+
+        // tạo bảng
+        const headerRow = worksheet.getRow(5);
+        const headers = [];
     }
-    
+
     async writeFileExcel(workbook: ExcelJS.Workbook, targetDir: string, fileName: string) {
         const fullPath = path.join(targetDir, fileName);
         if (!fs.existsSync(targetDir)) {
